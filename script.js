cdf3e79e5a83bb8c9cab29847166a93bd617e110
ChetnaD <71720709+ChetnaD20@users.noreply.github.com>
2021-08-02 17:22:18 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function hiName (){
+	var nAme = document.getElementById("name").value
+	document.getElementById("display").innerHTML= "Hello " + nAme + "!"
+}
+function lucifer () {
+	var best = document.getElementById("Deckerstar").value
+	alert("WRONG - There is nothing better than the Deckerstar couple")
+}
+function hoverGrow() {
+	var grow = document.getElementById("grow")
+	grow.style.fontSize="40px"
+}
